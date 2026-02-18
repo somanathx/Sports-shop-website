3e6a7780bd25a31cb8f74ad8fd336f8993d4cf43
Somanath <somanath1606@gmail.com>
2026-02-18 11:45:01 +0530

    account management

--- customers/urls.py
+++ customers/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('account',views.show_account,name='account')
+    path('account',views.show_account,name='account'),
+    path('logout',views.sign_out,name='logout')
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from . models import Customer
 
 # Create your views here.
+def sign_out(request):
+    logout(request)
+    return redirect('home')
+
 def show_account(request):
-    return render(request,"account.html")
+    context={}
+    if request.POST and 'register' in request.POST:
+        context['register']=True
+        try:
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            email=request.POST.get('email')
+            address=request.POST.get('address')
+            phone=request.POST.get('phone')
+        # create user accounts
+            user=User.objects.create_user(
+                username=username,
+                password=password,
+                email=email
+            )
+        # create customer account
+            customer=Customer.objects.create(
+                user=user,
+                phone=phone,
+                address=address
+            )
+            success_message="User registered successfully"
+            messages.success(request,success_message)
+        except Exception as e:
+            error_message="Duplicate username or invalid inputs"
+            messages.error(request,error_message)
+    if request.POST and 'login' in request.POST:
+        context['register']=False
+
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'invalid user credentials')   
+
+    return render(request,'account.html',context)
